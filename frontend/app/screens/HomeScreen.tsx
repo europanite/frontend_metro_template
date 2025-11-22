c57dbcbaaa4942d31b72774e4aba54636897efda
europanite <europanite@gmail.com>
2025-11-22 15:52:19 +0900

    fix/link

--- frontend/app/screens/HomeScreen.tsx
+++ frontend/app/screens/HomeScreen.tsx
@@ -1,6 +1,58 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import { useWindowDimensions } from "react-native";
+import { 
+  View, 
+  Text, 
+  Pressable, 
+  useWindowDimensions, 
+  ScrollView,
+  TouchableOpacity,
+  Linking 
+} from "react-native";
+import {REPO_URL} from "./HomeScreenUtil" 
 
 export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1 }}>
+      <View
+          style={{
+            alignSelf: "center",
+            width: "100%",
+            paddingHorizontal: 12,
+            paddingVertical: 10,
+            gap: 8,
+          }}
+      >
+        <TouchableOpacity onPress={() => Linking.openURL(REPO_URL)}>
+          <Text
+            style={{
+              fontSize: 24,
+              fontWeight: "800",
+              marginBottom: 12,
+              color: "#333333ff",
+              textDecorationLine: "underline",
+            }}
+          >
+            Frontend Metro Template
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
 
+  );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ frontend/app/screens/HomeScreenUtil.tsx
@@ -0,0 +1,2 @@
+export const REPO_URL      = "https://github.com/europanite/frontend_metro_template";
+export const REPO_URL_PAGE = "https://europanite.github.io/frontend_metro_template/";
